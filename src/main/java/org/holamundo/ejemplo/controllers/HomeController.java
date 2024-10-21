f5ac2774c26793a124049af74f94d31c4b8ea0b8
David <dsalcjim@myuax.com>
2024-10-21 13:08:02 +0200

    Añadir modificaciones en el controlador HomeController

--- src/main/java/org/holamundo/ejemplo/controllers/HomeController.java
+++ src/main/java/org/holamundo/ejemplo/controllers/HomeController.java
@@ -1,5 +1,17 @@
 package org.holamundo.ejemplo.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+
 //Controlador de la página principal
+
+@Controller
 public class HomeController {
+
+    @GetMapping("/")
+    public String home(Model model) {
+        model.addAttribute("message", "Welcome to the Home Page!");
+        return "home";
+    }
 }
